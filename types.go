0660d9d13d3afd5965685760932d984c32ff6afc
Michael Gasch <embano1@live.com>
2016-12-29 22:40:16 +0100

    Added v1.0 files for completeness again

--- types.go
+++ types.go
@@ -1,42 +1,25 @@
-// --- Type definitions ---
 package main
 
-// --- API service specs ---
-type servicelist struct {
+type ServiceList struct {
 	Kind       string `json:"kind"`
 	APIVersion string `json:"apiVersion"`
-	Items      []service
+	Items      []Service
 }
 
-type service struct {
-	Metadata struct {
-		Name string
-	} `json:"metadata"`
-	Spec spec
+type Service struct {
+	Metadata Metadata
+	Spec     Spec
 }
 
-type spec struct {
+type Spec struct {
 	Ports []struct {
 		Protocol   string `json:"protocol"`
 		Port       int    `json:"port"`
 		TargetPort int    `json:"targetPort"`
 		NodePort   int    `json:"nodePort"`
 	}
-	Type string `json:"type"`
 }
 
-// --- API node specs ---
-type nodelist struct {
-	Kind       string `json:"kind"`
-	APIVersion string `json:"apiVersion"`
-	Items      []node
-}
-
-type node struct {
-	Status struct {
-		Addresses []struct {
-			Type    string `json:"type"`
-			Address string `json:"address"`
-		}
-	}
+type Metadata struct {
+	Name string
 }
